d144102c486ee54622f2bf9335c766992ab12aa7
AnduinXue <anduin@aiursoft.com>
2023-11-28 09:13:46 +0000

    Try wired fix...

--- src/Aiursoft.ChessServer/Controllers/GamesController.cs
+++ src/Aiursoft.ChessServer/Controllers/GamesController.cs
@@ -66,7 +66,7 @@ public class GamesController : Controller
             subscription = channel.Subscribe(async t => { await _pusher.SendMessage(t.Content); });
             while (_pusher.Connected)
             {
-                await Task.Delay(int.MaxValue, HttpContext.RequestAborted);
+                await Task.Delay(int.MaxValue);
             }
         }
         finally
