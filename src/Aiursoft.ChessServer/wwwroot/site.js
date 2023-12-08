5512103ffdbfc073bd53d0d43bdd41c4ff601bbb
dvorak <dvorakchen@outlook.com>
2023-12-08 16:14:17 +0800

    add spectator

--- src/Aiursoft.ChessServer/wwwroot/site.js
+++ src/Aiursoft.ChessServer/wwwroot/site.js
@@ -1,107 +1,164 @@
 ﻿import { Chess } from "/node_modules/chess.js/dist/esm/chess.js";
 
-const statusControl = $('#status');
-const fenControl = $('#fen');
+const WHITE = "w";
+const BLACK = "b";
+const SPECTATOR = "";
+
+/**
+ * representing current Game
+ *
+ * ## example
+ * ```
+ * let gameId = 0;
+ * let game = new Game(gameId);
+ * ```
+ *
+ * @param {number} gameId current id of game
+ */
+function Game(gameId) {
+  this.gameId = gameId;
+  this.player = "";
+  this.chooseCharacterModal = new bootstrap.Modal(
+    document.getElementById("chooseCharacter")
+  );
+
+  this.chooseCharacter = () => {
+    this.chooseCharacterModal.show();
+  };
+
+  this.setPlayer = (player) => {
+    if ([WHITE, BLACK].includes(player)) {
+      this.player = player;
+    } else {
+      this.player = SPECTATOR;
+    }
+  };
+
+  /**
+   * let's start to play!
+   * but before, you need to choose a character
+   */
+  this.startGame = () => {
+    this.chooseCharacterModal.hide();
+
+    initGameBoard(this.player, this.gameId);
+
+    if (this.player === SPECTATOR) {
+      document.getElementById("board").style.cursor = "not-allowed";
+    }
+  };
+}
+
+const statusControl = $("#status");
+const fenControl = $("#fen");
 
 const initGameBoard = function (player, gameId) {
-    $.get("/games/" + gameId + ".fen", function (fen) {
-        let board = null;
-        let game = null;
+  $.get("/games/" + gameId + ".fen", function (fen) {
+    let board = null;
+    let game = null;
 
-        // Happens when a player picks up a piece.
-        function onDragStart(source, piece, position, _) {
-            // only allow moving players own pieces
-            if ((game.turn() !== player)) {
-                return false
-            }
+    // Happens when a player picks up a piece.
+    function onDragStart(source, piece, position, _) {
+      // only allow moving players own pieces
+      if (game.turn() !== player) {
+        return false;
+      }
 
-            // do not pick up pieces if the game is over
-            if (game.isGameOver()) return false
+      // do not pick up pieces if the game is over
+      if (game.isGameOver()) return false;
 
-            // only pick up pieces for the side to move
-            if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-                (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-                return false
-            }
+      // only pick up pieces for the side to move
+      if (
+        (game.turn() === "w" && piece.search(/^b/) !== -1) ||
+        (game.turn() === "b" && piece.search(/^w/) !== -1)
+      ) {
+        return false;
+      }
+    }
+
+    function onDrop(source, target) {
+      try {
+        const move = game.move({
+          from: source,
+          to: target,
+          promotion: "q",
+        });
+        if (move === null) {
+          return "snapback";
         }
+        const lastMove = game.history({ verbose: true }).pop().san;
+        $.post("/games/" + gameId + "/move/" + player + "/" + lastMove);
+      } catch (e) {
+        return "snapback";
+      }
+    }
 
-        function onDrop(source, target) {
-            try {
-                const move = game.move({
-                    from: source,
-                    to: target,
-                    promotion: 'q' 
-                })
-                if (move === null) {
-                    return 'snapback'
-                }
-                const lastMove = game.history({verbose: true}).pop().san;
-                $.post("/games/" + gameId + "/move/" + player + "/" + lastMove);
-            } catch (e) {
-                return 'snapback';
-            }
+    // Hack to make sure castling\promotion works.
+    function onSnapEnd() {
+      board.position(game.fen());
+    }
+
+    function updateStatusText() {
+      let status;
+      let moveColor = "White";
+      if (game.turn() === "b") {
+        moveColor = "Black";
+      }
+      if (game.isCheckmate()) {
+        status =
+          "Game over, " +
+          moveColor +
+          " is in checkmate, and winner is " +
+          (game.turn() === "w" ? "Black" : "White");
+      } else if (game.isDraw()) {
+        status = "Game over, drawn position";
+      } else {
+        status = moveColor + " to move";
+        if (game.isCheck()) {
+          status += ", " + moveColor + " is in check";
         }
+      }
+      statusControl.html(status);
+      fenControl.html(game.fen());
+    }
 
-        // Hack to make sure castling\promotion works.
-        function onSnapEnd() {
-            board.position(game.fen())
-        }
+    const config = {
+      orientation: player === "w" ? "white" : "black",
+      draggable: true,
+      dragoffBoard: "snapback",
+      position: fen,
+      onDragStart: onDragStart,
+      onSnapEnd: onSnapEnd,
+      onDrop: onDrop,
+    };
+    board = ChessBoard("board", config);
 
-        function updateStatusText() {
-            let status;
-            let moveColor = 'White';
-            if (game.turn() === 'b') {
-                moveColor = 'Black';
-            } if (game.isCheckmate()) {
-                status = 'Game over, ' + moveColor + ' is in checkmate, and winner is ' + (game.turn() === 'w' ? 'Black' : 'White');
-            } else if (game.isDraw()) {
-                status = 'Game over, drawn position';
-            } else {
-                status = moveColor + ' to move';
-                if (game.isCheck()) {
-                    status += ', ' + moveColor + ' is in check';
-                }
-            }
-            statusControl.html(status);
-            fenControl.html(game.fen());
-        }
+    function refresh(newFen) {
+      game = new Chess(newFen);
+      board.position(newFen);
+      console.log("Got fen " + newFen + ". refreshing board...");
+      updateStatusText();
+    }
 
-        const config = {
-            orientation: player === "w" ? "white" : "black",
-            draggable: true,
-            dragoffBoard: 'snapback',
-            position: fen,
-            onDragStart: onDragStart,
-            onSnapEnd: onSnapEnd,
-            onDrop: onDrop
-        };
-        board = ChessBoard('board', config);
+    refresh(fen);
 
-        function refresh(newFen) {
-            game = new Chess(newFen);
-            board.position(newFen);
-            console.log("Got fen " + newFen + ". refreshing board...");
-            updateStatusText();
-        }
+    const wsScheme = window.location.protocol === "https:" ? "wss://" : "ws://";
+    const socket = new WebSocket(
+      wsScheme + window.location.host + "/games/" + gameId + ".ws"
+    );
+    socket.onmessage = function (event) {
+      refresh(event.data);
+    };
 
-        refresh(fen);
-        
-        const wsScheme = window.location.protocol === "https:" ? "wss://" : "ws://";
-        const socket = new WebSocket(wsScheme + window.location.host + "/games/" + gameId + ".ws");
-        socket.onmessage = function (event) {
-            refresh(event.data);
-        };
-        
-        // Auto reconnect.
-        socket.onclose = function () {
-            alert("Socket closed. Reconnecting...");
-            setTimeout(function () {
-                initGameBoard(player, gameId);
-            }, 1000);
-        };
-    });
-
+    // Auto reconnect.
+    socket.onclose = function () {
+      alert("Socket closed. Reconnecting...");
+      setTimeout(function () {
+        initGameBoard(player, gameId);
+      }, 1000);
+    };
+  });
 };
 
 // noinspection JSUnusedGlobalSymbols
-export default initGameBoard;
+export default Game;
