3082701c5508660cdf607c7e54895b069e258935
xuef <xuef@microsoft.com>
2023-12-30 15:04:39 +0000

    WIth npm.

--- install.sh
+++ install.sh
@@ -14,8 +14,10 @@ install()
     echo "Installing $app_name... to port $port"
     
     aiur install/dotnet
+    aiur install/node
     aiur git/clone_to $repo_path /tmp/repo
 
+    npm install --prefix /tmp/repo/src/Aiursoft.ChessServer/wwwroot/
     aiur dotnet/publish "/tmp/repo/$proj_path" "/opt/apps/$app_name"
     aiur services/register_aspnet_service $app_name $port "/opt/apps/$app_name" $dll_name
 
