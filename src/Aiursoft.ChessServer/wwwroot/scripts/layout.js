0105d7079a7160f932087872cbb77a12b11476b1
AnduinXue <anduin@aiursoft.com>
2024-01-01 07:31:24 +0000

    New chess.

---
+++ src/Aiursoft.ChessServer/wwwroot/scripts/layout.js
@@ -0,0 +1,20 @@
+import { autoTheme } from "/node_modules/@aiursoft/autodark.js/dist/esm/autodark.js";
+import { getUserName, changeName } from "/scripts/player.js";
+autoTheme();
+
+async function loadName() {
+  document.getElementById("player-nick-name").innerText = await getUserName();
+}
+
+async function promptChangeName() {
+  const newName = prompt("Please enter your new name:");
+  if (newName) {
+    await changeName(newName);
+    await loadName();
+  }
+}
+
+await loadName();
+document
+  .getElementById("player-nick-name")
+  .addEventListener("click", promptChangeName);
