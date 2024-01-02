91b01b634146c4425f21c993e9da10771d1a2b15
Anduin Xue <xuef@microsoft.com>
2024-01-02 05:48:45 +0000

    Add challenge APIs.

--- src/Aiursoft.ChessServer/Models/Game.cs
+++ src/Aiursoft.ChessServer/Models/Game.cs
@@ -11,3 +11,29 @@ public class Game
     
     public object MovePieceLock { get; } = new();
 }
+
+public enum RoleRule
+{
+    CreatorWhite,
+    AccepterWhite,
+    Random
+}
+
+public enum ChallengePermission
+{
+    Public,
+    Unlisted,
+}
+
+public class Challenge
+{
+    public Guid CreatorId { get; set; }
+
+    public Guid? AccepterId { get; set; } = null;
+    
+    public RoleRule RoleRule { get; set; } = RoleRule.Random;
+    
+    public TimeSpan TimeLimit { get; set; } = TimeSpan.FromMinutes(10);
+    
+    public ChallengePermission Permission { get; set; } = ChallengePermission.Public;
+}
